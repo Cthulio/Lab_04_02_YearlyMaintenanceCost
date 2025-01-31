a6c8c9f677e6de4b4f42bea5b479093eea95da77
Nolan Crone <crone.nolan@gmail.com>
2025-01-30 22:33:55 -0500

    fixing a tab

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         double costOfSummer = 2105.11;
         double costOfAutumn = 2245.01;
         double costOfWinter = 2859.91;
-                double yearlyMaintenanceCost= costOfSpring+costOfSummer+costOfAutumn+costOfWinter;
+        double yearlyMaintenanceCost= costOfSpring+costOfSummer+costOfAutumn+costOfWinter;
         System.out.println("The total cost of maintenance for all seasons, (Spring):"+costOfSpring+" (Summer):"
                 +costOfSummer+" (Autumn):"+costOfAutumn+" and (Winter):"+costOfWinter+", is: "+yearlyMaintenanceCost);
     }
